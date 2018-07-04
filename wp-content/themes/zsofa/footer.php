ccc3c9c60a6716c940d38a979ce1605188302100
Dang Quyen <dangquyen@fgct.net>
2018-07-05 04:04:41 +0700

    product video

--- wp-content/themes/zsofa/footer.php
+++ wp-content/themes/zsofa/footer.php
@@ -112,6 +112,9 @@
 
     </div>
 </div>
+<script>
+    popup.closePopup('#overlay-region', ".win-wrapper-vdproduct");
+</script>
 <script>(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
