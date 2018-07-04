fffb8e4a5839ed8b56bdf3dfc7e5df9471afbb00
Dang Quyen <dangquyen@fgct.net>
2018-07-05 03:09:26 +0700

    product video

--- wp-content/themes/zsofa/template-homepage.php
+++ wp-content/themes/zsofa/template-homepage.php
@@ -121,10 +121,10 @@ get_header(); ?>
 						<div class="col-md-6">
 							<div class="videos-main-product">
                                 <?php
-                                $args = array( 'post_type' => 'product', 'posts_per_page' => 1, 'product_cat' => 'video-product', 'orderby' => 'rand' );
+                                $args = array( 'post_type' => 'product', 'posts_per_page' => 1, 'product_cat' => 'video-product', 'orderby' => 'none' );
                                 $loop = new WP_Query( $args );
                                 while ( $loop->have_posts() ) : $loop->the_post(); global $product; ?>
-                                    <?php if(get_field_details('main_video') == '1'): ?>
+                                    <?php if(get_field_details('main_video') == 1): ?>
                                         <a class="img-wrapper" href="<?php echo get_permalink( $loop->post->ID ) ?>"><?php if (has_post_thumbnail( $loop->post->ID )) echo get_the_post_thumbnail($loop->post->ID, 'post-thumb-wide'); else echo '<img src="'.woocommerce_placeholder_img_src().'" width="600px" height="400px" />'; ?></a>
                                         <a href="javascript:;" class="view-more">Xem video</a>
                                         <div class="video-product-title"><a href="<?php echo get_permalink( $loop->post->ID ) ?>">Bàn ăn 8 ghế mã T1723</a></div>
