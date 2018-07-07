1ea5e35d8069b079fe2db0a1af62f441687732e8
Dang Quyen <dangquyen@fgct.net>
2018-07-08 02:37:39 +0700

    update archive sections

---
+++ wp-content/themes/zsofa/template-parts/news-letter.php
@@ -0,0 +1,24 @@
+<div class="section9 newsletter">
+    <div class="mobile-newsletter">
+        <form action="" class="newsletter-form">
+            <img src="" alt="" class="img-responsive">
+            <input type="email" placeholder="Nhập địa chỉ email của bạn">
+            <input class="mobile-submit" type="submit" value="Đăng ký">
+        </form>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5">
+                <form action="" class="newsletter-form">
+                    <img src="" alt="" class="img-responsive">
+                    <input type="email" placeholder="Nhập địa chỉ email của bạn vào đây">
+                    <input class="desktop-submit" type="submit" value="Đăng ký">
+                </form>
+
+            </div>
+            <div class="col-md-7">
+                <img class="img-responsive" src="<?php echo get_template_directory_uri() ; ?>/assets/images/169.jpg" alt="" class="img-responsive">
+            </div>
+        </div>
+    </div>
+</div>
---
+++ wp-content/themes/zsofa/template-parts/store-news.php
@@ -0,0 +1,55 @@
+<div class="section8 store-news">
+    <div class="container">
+        <div class="row">
+            <div class="col-ssmm-12 col-ssm-12 col-md-4">
+                <h1 class="store-news-title">
+                    <a href="">CHUYÊN ĐỀ ZSOFA</a>
+                </h1>
+                <div class="more-link"><a href="">Bàn giao nội thất cho anh Long ở FLC Thanh Hóa</a></div>
+                <div class="news-content-wr">
+                    <a href=""><img class="img-responsive" src="<?php echo get_template_directory_uri() ; ?>/assets/images/ban-giao-noi-that-cho-anh-long-o-flc-thanh-hoa.jpg" alt=""></a><p class="news-summary">Thương hiệu nội thất ERADO vô cùng vui mừng khi anh Long ở FLC Thanh Hóa đã quyết định chọn nội thất cho căn nhà của </p>
+                </div>
+                <div class="other-news">
+                    <h5 class="other-news-title">Các tin khác</h5>
+                    <ul>
+                        <li><a href="">Bàn giao nội thất cho gia đình anh Dũng - P1013, Khu </a></li>
+                        <li><a href="">Bàn giao sofa cho chị Thúy ở Lò Đúc</a></li>
+                        <li><a href="">Bàn giao sofa cho Chị Tân ở KĐT Yên Hòa, Trung Yên </a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-ssmm-12 col-ssm-12 col-md-4">
+                <h1 class="store-news-title">
+                    <a href="">Kinh nghiệm hay của Zsofa</a>
+                </h1>
+                <div class="more-link"><a href="">Chọn ghế bàn ăn loại nào</a></div>
+                <div class="news-content-wr">
+                    <a href=""><img class="img-responsive" src="<?php echo get_template_directory_uri() ; ?>/assets/images/nen-mua-ban-ghe-an-loai-nao.jpg" alt=""></a><p class="news-summary">Ghế ăn hiện đại với khung thép cứng và được bọc da hoàn toàn vô cùng hiện đại và sang trọng cũng như tốt cho sức khỏe của gia đình. </p>
+                </div>
+                <div class="other-news">
+                    <h5 class="other-news-title">Các tin khác</h5>
+                    <ul>
+                        <li><a href="">Chọn ghế ngồi ăn cho căn hộ nhỏ</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-ssmm-12 col-ssm-12 col-md-4">
+                <h1 class="store-news-title">
+                    <a href="">Chuyên đề nội thất</a>
+                </h1>
+                <div class="more-link"><a href="">Sức hút từ bộ sưu tập sofa da thật phối gỗ của thương </a></div>
+                <div class="news-content-wr">
+                    <a href=""><img class="img-responsive" src="<?php echo get_template_directory_uri() ; ?>/assets/images/sofa-da-that-phoi-go-den-tu-thuong-hieu-noi-that-erado.png" alt=""></a><p class="news-summary">Thương hiệu nội thất ZSOFA giới thiệu đến quý khách hàng bộ sưu tập sofa da thật phối gỗ hiện đại đang được rất nhiều khách hàng </p>
+                </div>
+                <div class="other-news">
+                    <h5 class="other-news-title">Các tin khác</h5>
+                    <ul>
+                        <li><a href="">Bàn giao nội thất cho gia đình anh Dũng - P1013, Khu </a></li>
+                        <li><a href="">Bàn giao sofa cho chị Thúy ở Lò Đúc</a></li>
+                        <li><a href="">Bàn giao sofa cho Chị Tân ở KĐT Yên Hòa, Trung Yên </a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
