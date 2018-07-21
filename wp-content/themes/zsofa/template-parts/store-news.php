8f0a6ef873533a6f0797bee7afc33f83965c8dc8
Dang Quyen <dangquyen@fgct.net>
2018-07-21 18:38:55 +0700

    fix slug name category

--- wp-content/themes/zsofa/template-parts/store-news.php
+++ wp-content/themes/zsofa/template-parts/store-news.php
@@ -6,7 +6,7 @@
                     <a href="">CHUYÊN ĐỀ ZSOFA</a>
                 </h1>
                 <?php
-                $array = array('posts_per_page' => 4,'category_name'=> 'chuyen-de-zsofa','orderby'=> 'date');
+                $array = array('posts_per_page' => 4,'name'=> 'chuyen-de-zsofa','orderby'=> 'date');
                 $latest_posts = get_posts( $args );
                 if ( $latest_posts ) : $post_count = 0;
                 foreach ( $latest_posts as $post ) :
@@ -37,7 +37,7 @@
                     <a href="">Kinh nghiệm hay của Zsofa</a>
                 </h1>
                 <?php
-                $array = array('posts_per_page' => 4,'category_name'=> 'kinh-nghiem-hay-cua-zsofa','orderby'=> 'date');
+                $array = array('posts_per_page' => 4,'name'=> 'kinh-nghiem-hay-cua-zsofa','orderby'=> 'date');
                 $latest_posts = get_posts( $args );
                 if ( $latest_posts ) : $post_count = 0;
                 foreach ( $latest_posts as $post ) :
@@ -68,7 +68,7 @@
                     <a href="">CHUYÊN ĐỀ NỘI THẤT</a>
                 </h1>
                 <?php
-                $array = array('posts_per_page' => 4,'category_name'=> 'chuyen-de-noi-that','orderby'=> 'date');
+                $array = array('posts_per_page' => 4,'name'=> 'chuyen-de-noi-that','orderby'=> 'date');
                 $latest_posts = get_posts( $args );
                 if ( $latest_posts ) : $post_count = 0;
                 foreach ( $latest_posts as $post ) :
