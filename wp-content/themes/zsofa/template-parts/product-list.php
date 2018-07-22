9e94c1d6250262eb81fd3b36eda5f751a5aa0752
Dang Quyen <dangquyen@fgct.net>
2018-07-23 01:40:14 +0700

    update link category

--- wp-content/themes/zsofa/template-parts/product-list.php
+++ wp-content/themes/zsofa/template-parts/product-list.php
@@ -1,3 +1,44 @@
+<?php
+switch ($product_cat){
+    case 'ghe-sofa':
+        $sub_categories = array('Sofa khuyến mại'=>'/ghe-sofa-khuyen-mai/',
+            'Sofa Bed'=>'/danh-muc/sofa-giuong/',
+            'Sofa khuyến mại'=>'/ghe-sofa-khuyen-mai/',
+            'Sofa karaoke'=>'/danh-muc/sofa-cafe-karaoke/',
+            'Sofa cafe'=>'/danh-muc/ghe-sofa-cafe/',
+            'Sofa băng'=>'/danh-muc/sofa-bang/',
+            'Sofa góc'=>'/danh-muc/sofa-goc-gia-re/',
+            'Sofa phòng khách'=>'/danh-muc/sofa-phong-khach/',
+            'Sofa văn phòng'=>'/danh-muc/sofa-van-phong/',
+            'Sofa cao cấp'=>'/danh-muc/ghe-sofa-cao-cap/'
+            );
+        break;
+    case 'noi-that':
+        $sub_categories = array('Tư vấn mua Sofa'=>'/quy-trinh-tu-van-khach-hang-cua-zsofa-vn/',
+            'Thiết kế logo công ty' => '/thiet-ke-logo.html',
+            'Thiết kế nội thất chung cư' => 'https://rubeedecor.com/dich-vu/thiet-ke-noi-that-chung-cu.html',
+            'Thiết kế nội thất văn phòng' => 'https://rubeedecor.com/dich-vu/thiet-ke-noi-that-van-phong.html',
+            'Thiết kế nội thất nhà hàng' => 'https://rubeedecor.com/dich-vu/thiet-ke-noi-that-nha-hang.html'
+        );
+        break;
+    case 'sofa-giuong':
+        $sub_categories = array('Sofa giảm giá'=>'/sofa-dang-giam-gia-tai-zsofa-vn/',
+            'Ghế sofa làm nail'=>'/ghe-sofa-lam-nail-cao-cap-bao-hanh-1-5-nam/',
+            'Cho thuê ghế Sofa'=>'/cho-thue-ghe-sofa/',
+            'Bàn ghế sofa giá rẻ'=>'/danh-muc/ghe-sofa-cafe/',
+            'Sof mini giá rẻ'=>'/sofa-mini-gia-re-tphcm-tu-zsofa/'
+        );
+        break;
+    case 'ban-sofa':
+        $sub_categories = array('Bàn sofa'=>'/danh-muc/ban-sofa/'
+        );
+        break;
+    case 'tham-sofa':
+        $sub_categories = array('Thảm sofa'=>'/danh-muc/tham-sofa/'
+        );
+        break;
+}
+?>
 <div class="featured-category <?php echo $category_class?>">
     <div class="container">
         <div class="category-title-pr">
@@ -12,16 +53,12 @@
                 </div>
                 <div class="col-md-9">
                     <ul class="sub-categories float-right">
-                        <li><a href="">Sofa nỉ</a></li>
-                        <li><a href="">Sofa da</a></li>
-                        <li><a href="">Sofa vải</a></li>
-                        <li><a href="">Sofa góc</a></li>
-                        <li><a href="">Sofa hà nội</a></li>
-                        <li><a href="">Sofa cổ điển</a></li>
-                        <li><a href="">Sofa gia đình</a></li>
-                        <li><a href="">Sofa hàn quốc</a></li>
-                        <li><a href="">Sofa phòng khách</a></li>
-                        <li><a href="">Sofa đẹp</a></li>
+                        <?php
+                            foreach ($sub_categories as $name_cat => $cate_link)
+                            {
+                                echo "<li><a href='$cate_link'>$name_cat</a></li>";
+                            }
+                        ?>
                     </ul>
                 </div>
             </div>
