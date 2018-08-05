e6d638ae16401b0e5aecb2498f874fc285a1b039
Dang Quyen <dangquyen@fgct.net>
2018-08-05 11:41:27 +0700

    facebook comments

--- wp-content/themes/zsofa/footer.php
+++ wp-content/themes/zsofa/footer.php
@@ -114,7 +114,7 @@
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
         js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v3.1&appId=2023601647852009&autoLogAppEvents=1';
+        js.src = 'https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v3.1&appId=768568736686310&autoLogAppEvents=1';
         fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));</script>
 </body>
