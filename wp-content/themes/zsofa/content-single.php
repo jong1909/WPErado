a413a19ab1056969289a9ad447f85025c1e4368b
Dang Quyen <dangquyen@fgct.net>
2018-08-05 16:05:58 +0700

    news on content single page

--- wp-content/themes/zsofa/content-single.php
+++ wp-content/themes/zsofa/content-single.php
@@ -245,80 +245,9 @@
 <?php get_template_part('template-parts/product', 'video'); ?>
 <?php get_template_part('template-parts/store', 'services'); ?>
 <?php get_template_part('template-parts/product', 'categories'); ?>
-<div class="section8 store-news">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4">
-                <h1 class="store-news-title">
-                    <a href="">CHUYÊN ĐỀ ERADO</a>
-                </h1>
-                <div class="more-link"><a href="">Bàn giao nội thất cho anh Long ở FLC Thanh Hóa</a></div>
-                <div class="news-content-wr">
-                    <a href=""><img class="img-responsive"
-                                    src="<?php echo get_template_directory_uri(); ?>/assets/images/ban-giao-noi-that-cho-anh-long-o-flc-thanh-hoa.jpg"
-                                    alt=""></a>
-                    <p class="news-summary">Thương hiệu nội thất ERADO vô cùng vui mừng khi anh Long ở FLC Thanh Hóa đã
-                        quyết định chọn nội thất cho căn nhà của </p>
-                </div>
-                <div class="other-news">
-                    <h5 class="other-news-title">Các tin khác</h5>
-                    <ul>
-                        <li><a href="">Bàn giao nội thất cho gia đình anh Dũng - P1013, Khu </a></li>
-                        <li><a href="">Bàn giao sofa cho chị Thúy ở Lò Đúc</a></li>
-                        <li><a href="">Bàn giao sofa cho Chị Tân ở KĐT Yên Hòa, Trung Yên </a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <h1 class="store-news-title">
-                    <a href="">Kinh nghiệm hay của erado</a>
-                </h1>
-                <div class="more-link"><a href="">Chọn ghế bàn ăn loại nào</a></div>
-                <div class="news-content-wr">
-                    <a href=""><img class="img-responsive"
-                                    src="<?php echo get_template_directory_uri(); ?>/assets/images/nen-mua-ban-ghe-an-loai-nao.jpg"
-                                    alt=""></a>
-                    <p class="news-summary">Ghế ăn hiện đại với khung thép cứng và được bọc da hoàn toàn vô cùng hiện
-                        đại và sang trọng cũng như tốt cho sức khỏe của gia đình. </p>
-                </div>
-                <div class="other-news">
-                    <h5 class="other-news-title">Các tin khác</h5>
-                    <ul>
-                        <li><a href="">Chọn ghế ngồi ăn cho căn hộ nhỏ</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <h1 class="store-news-title">
-                    <a href="">Chuyên đề nội thất</a>
-                </h1>
-                <div class="more-link"><a href="">Sức hút từ bộ sưu tập sofa da thật phối gỗ của thương </a></div>
-                <div class="news-content-wr">
-                    <a href=""><img class="img-responsive"
-                                    src="<?php echo get_template_directory_uri(); ?>/assets/images/sofa-da-that-phoi-go-den-tu-thuong-hieu-noi-that-erado.png"
-                                    alt=""></a>
-                    <p class="news-summary">Thương hiệu nội thất ERADO giới thiệu đến quý khách hàng bộ sưu tập sofa da
-                        thật phối gỗ hiện đại đang được rất nhiều khách hàng </p>
-                </div>
-                <div class="other-news">
-                    <h5 class="other-news-title">Các tin khác</h5>
-                    <ul>
-                        <li><a href="">Bàn giao nội thất cho gia đình anh Dũng - P1013, Khu </a></li>
-                        <li><a href="">Bàn giao sofa cho chị Thúy ở Lò Đúc</a></li>
-                        <li><a href="">Bàn giao sofa cho Chị Tân ở KĐT Yên Hòa, Trung Yên </a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+<?php get_template_part( 'template-parts/store','news' ); ?>
 <div class="store-connect-mobile">
-    <h3>Kết nối với Erado</h3>
+    <h3>Kết nối với ZSOFA</h3>
 </div>
-<div class="fb-page" data-href="https://www.facebook.com/zsofavn2/" data-tabs="timeline" data-small-header="false"
-     data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
-    <blockquote cite="https://www.facebook.com/zsofavn2/" class="fb-xfbml-parse-ignore"><a
-                href="https://www.facebook.com/zsofavn2/">zsofa.vn - Gò Vấp - Hệ thống bán sỉ và lẻ ghế sofa.</a>
-    </blockquote>
-</div>
+<div class="fb-page" data-href="https://www.facebook.com/zsofavn2/" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/zsofavn2/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/zsofavn2/">zsofa.vn - Gò Vấp - Hệ thống bán sỉ và lẻ ghế sofa.</a></blockquote></div>
 <?php get_template_part('template-parts/news', 'letter'); ?>
