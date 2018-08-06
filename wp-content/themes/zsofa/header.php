a1062f69be4370180b00349b8c2c93f7e22b5015
Dang Quyen <dangquyen@fgct.net>
2018-08-06 16:42:14 +0700

    fix redirect

--- wp-content/themes/zsofa/header.php
+++ wp-content/themes/zsofa/header.php
@@ -80,7 +80,7 @@
                     <a class="hotline-num" href="tel:0933322804">&nbsp;</a>
                 </div>
                 <div class="col-md-3 col-ssmm-6 col-ssm-6 shop-logo">
-                    <a href=""><img src="<?php echo get_template_directory_uri() ; ?>/assets/images/zsofa_logo.png" class="img-responsive" alt=""></a>
+                    <a href="/"><img src="<?php echo get_template_directory_uri() ; ?>/assets/images/zsofa_logo.png" class="img-responsive" alt=""></a>
                 </div>
                 <div class="col-ssmm-3 col-ssm-3 show-destop-screen">
                     <a href=""><img src="<?php echo get_template_directory_uri() ; ?>/assets/images/pc.png" alt=""></a>
